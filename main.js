3a5067905a6c4786fd75dfc3c60f928fbf4745eb
ADCByDefault <swaran9938@gmail.com>
2024-08-31 04:20:43 +0200

    to fox sun moon  position

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
+import * as Utils from "./utils";
 import * as THREE from "three";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import SunCalc from "suncalc";
 
-// Initialize the scene, camera, and renderer
+// Scene camera and renderer
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
-    75,
+    40,
     window.innerWidth / window.innerHeight,
     0.1,
     1000
@@ -16,46 +16,89 @@ const renderer = new THREE.WebGLRenderer({
     antialias: true,
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFShadowMap;
 
-// Orbit controls for camera interaction
+// Orbit controls
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
-controls.dampingFactor = 0.25;
+controls.dampingFactor = 0.1;
+controls.enablePan = true;
+controls.enableRotate = true;
+controls.enableZoon = true;
+camera.position.set(10, 3, 5);
+controls.update();
 
-// Directional light (sunlight)
+// Global Illumination
 const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
 directionalLight.castShadow = true;
-directionalLight.shadow.bias = -0.003; // Shadow bias for softer shadows
-directionalLight.shadow.mapSize.width = 2048; // High resolution shadows
-directionalLight.shadow.mapSize.height = 2048;
-scene.add(directionalLight);
+directionalLight.shadow.bias = -0.003;
+directionalLight.shadow.mapSize.width = 8192;
+directionalLight.shadow.mapSize.height = 8192;
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.01);
+scene.add(directionalLight, ambientLight);
 
-// Dense grid
+// Grid for debug
 const gridHelper = new THREE.GridHelper(200, 400, 0x888888, 0x444444);
 scene.add(gridHelper);
 
-// Helper function: Convert azimuth and altitude (elevation) to Cartesian coordinates
-function azimuthAltitudeToCartesian(azimuth, altitude, distance = 1) {
-    const x = distance * Math.cos(altitudeRad) * Math.sin(azimuthRad);
-    const y = distance * Math.cos(altitudeRad) * Math.cos(azimuthRad);
-    const z = distance * Math.sin(altitudeRad);
+// Meshes and user's position
+const [earth, sun, moon, userPosition] = await Promise.all([
+    Utils.loadModel("./models/Earth.glb", 1 / 500),
+    Utils.loadModel("./models/Sun.glb", 1 / 500),
+    Utils.loadModel("./models/Moon.glb", 1 / 1000),
+    Utils.getCurrentPosition(),
+]);
+earth.rotateY(Utils.degToRad(-90));
+scene.add(earth, moon, sun);
 
-    return new THREE.Vector3(x, y, z);
+// Animation
+let animationRequest = null;
+animate();
+function animate() {
+    animationRequest = requestAnimationFrame(animate);
+    const latitude = 0;
+    const longitude = 0;
+    // const date = new Date(2024, 3, 8, 20, 20);
+    const date = new Date();
+
+    let spos = SunCalc.getPosition(date, latitude, longitude);
+    let mpos = SunCalc.getMoonPosition(date, latitude, longitude);
+
+    let sunPosition = Utils.horizontalToCartesian(
+        spos.azimuth,
+        spos.altitude,
+        800
+    );
+    let moonPosition = Utils.horizontalToCartesian(
+        mpos.azimuth,
+        mpos.altitude,
+        20
+    );
+    console.log(spos)
+    setSun(sunPosition);
+    setMoon(moonPosition);
+
+    controls.update();
+    renderer.render(scene, camera);
 }
 
-// function degToRad(deg) {
-//     return (deg * Math.PI) / 180;
-// }
-// function radToDeg(rad) {
-//     return (rad * 180) / Math.PI;
-// }
+/**
+ *
+ * @param {THREE.Vector3} position
+ */
+function setMoon(position) {
+    moon.position.copy(position);
+    moon.lookAt(0, 0, 0);
+    moon.rotateY(Utils.degToRad(90));
+}
 
-const lat = 36,
-    long = -89;
-console.log(SunCalc);
-const date = new Date(2024, 3, 8, 19, 18);
-console.log(date);
-const pos = SunCalc.getPosition(date, lat, long);
-const mpos = SunCalc.getMoonPosition(date, lat, long);
-console.log(pos, mpos);
+/**
+ *
+ * @param {THREE.Vector3} position
+ */
+function setSun(position) {
+    sun.position.copy(position);
+    const lightPosition = position.multiplyScalar(0.5);
+    directionalLight.position.copy(lightPosition);
+}
--- utils.js
+++ utils.js
@@ -1,10 +1,19 @@
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+export const Position = {
+    latitude: null,
+    longitude: null,
+    latitudeRad: null,
+    longitudeRad: null,
+    navigatorPosition: null,
+    ipapiData: null,
+    error: true,
+};
+
 export async function getCurrentPosition() {
     const ret = {
-        latitude: null,
-        longitude: null,
-        navigatorPosition: null,
-        ipapiData: null,
-        error: true,
+        ...Position,
     };
     if (navigator.geolocation) {
         await new Promise((resolve, reject) => {
@@ -33,5 +42,69 @@ export async function getCurrentPosition() {
     ret.longitude = ret.navigatorPosition
         ? ret.navigatorPosition.coords.longitude
         : data.longitude;
+    ret.latitudeRad = degToRad(ret.latitude);
+    ret.longitudeRad = degToRad(ret.longitude);
     return ret;
+}
+export function degToRad(deg) {
+    return (deg * Math.PI) / 180;
+}
+export function radToDeg(rad) {
+    return (rad * 180) / Math.PI;
+}
+
+/**
+ *
+ * @param {string} path
+ * @param {number} scale
+ * @returns {Promise<THREE.Object3D>}
+ */
+export async function loadModel(path, scale = 1) {
+    const loader = new GLTFLoader();
+    return new Promise((resolve, reject) => {
+        loader.load(
+            path,
+            (data) => {
+                data.scene.traverse((node) => {
+                    node.receiveShadow = true;
+                    node.castShadow = true;
+                });
+                data.scene.scale.set(scale, scale, scale);
+                resolve(data.scene);
+            },
+            (progress) => {},
+            (error) => {
+                reject(error);
+            }
+        );
+    });
+}
+
+/**
+ *
+ * @param {number} azimuth
+ * @param {number} altitude
+ * @param {number} distance
+ * @returns {THREE.Vector3}
+ */
+export function horizontalToCartesian(azimuth, altitude, distance = 1) {
+    const x = distance * Math.sin(altitude) * Math.cos(azimuth);
+    const y = distance * Math.sin(altitude) * Math.sin(azimuth);
+    const z = distance * Math.cos(altitude);
+
+    return new THREE.Vector3(x, y, z);
+}
+
+/**
+ * long = 0 , lat = 0, distance = 1 corrisponds to xyz (1, 0, 0)
+ * @param {number} latitude
+ * @param {number} longitude
+ * @param {number} distance
+ * @returns {THREE.Vector3}
+ */
+export function geographicToCartesian(latitude, longitude, distance = 1) {
+    const x = Math.cos(latitude) * Math.cos(longitude);
+    const y = Math.sin(latitude);
+    const z = -Math.cos(latitude) * Math.sin(longitude);
+    return new THREE.Vector3(x, y, z);
 }
