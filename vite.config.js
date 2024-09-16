c4513c3523f420cc7e82fbfa92558c809f5bec01
ADCByDefault <swaran9938@gmail.com>
2024-09-16 17:03:56 +0200

    added allias in vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ export default defineConfig({
         target: "es2022",
     },
     base: "/times/",
+    resolve: {
+        alias: {
+            three: "./node_modules/three/three.module.js",
+            suncalc: "./node_modules/suncalc.js",
+        },
+    },
 });
